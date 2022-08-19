3f86cef024d418449d00a4339cad1288e64d1d4d
LeipreachanTeo <pokiboom@mail.ru>
2022-08-19 15:30:52 +0700

    second task

---
+++ task_two/task_two.cs
@@ -0,0 +1,16 @@
+﻿int a = Int32.Parse(Console.ReadLine());
+int b = Int32.Parse(Console.ReadLine());
+int c = Int32.Parse(Console.ReadLine());
+
+if (a > b && a > c)
+{
+    Console.WriteLine($"max = {a}");
+}
+else if (b > c && b > a)
+{
+    Console.WriteLine($"max = {b}");
+}
+else
+{
+    Console.WriteLine($"max = {c}");
+}
