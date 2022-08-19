4a1185f47e26efff50c6069ff6a3f810d32099b1
LeipreachanTeo <pokiboom@mail.ru>
2022-08-19 14:26:55 +0700

    first task

---
+++ Day_one/task_one.cs
@@ -0,0 +1,11 @@
+﻿int a = Int32.Parse(Console.ReadLine());
+int b = Int32.Parse(Console.ReadLine());
+
+if (a > b)
+{
+    Console.WriteLine($"max = {a}");
+}
+else
+{
+    Console.WriteLine($"max = {b}");
+}
